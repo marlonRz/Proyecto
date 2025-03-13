bc1fc2ca0c27b2061c44a8dd6d7f03253bb695b5
Marlon Stiven <marlonsteven930@gmail.com>
2025-03-13 12:55:57 -0500

    transition

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,17 @@ import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 import Floor from './models-3d/Floor';
 import Skull3d from './models-3d/Skull';
+import { motion } from "framer-motion"; 
 
 function App() {
   return (
     <div className='information-start'>
-      <section className='section-start'>
+      <motion.section
+        className='section-start'
+        initial={{ x: "-100vw", opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{ type: "tween", duration: 0.8 }}
+      >
         <h1 className='information-start-h1'>
           GUIATE CON NOSOTROS, TU SALUD EN UN SOLO LUGAR.
         </h1>
@@ -20,7 +26,7 @@ function App() {
           Observa tu anatomía con modelos 3D, aprende más sobre tus enfermedades y funcionamiento de las partes de tu cuerpo.
         </p>
         <Button text="Ver más Enfermedades" width='250px' height='50px' />
-      </section>
+      </motion.section>
 
       <div className="object-3d">
         <Canvas
@@ -30,7 +36,7 @@ function App() {
         >
           <ambientLight intensity={1.5} />
           <directionalLight position={[5, 5, 10]} intensity={2} />
-          <Skull3d  position={[0, 1, 0]} scale={[10, 10, 10]} />
+          <Skull3d position={[0, 1, 0]} scale={[10, 10, 10]} />
           <OrbitControls />
         </Canvas>
       </div>
@@ -39,5 +45,6 @@ function App() {
 }
 
 export default App;
+
 
 
