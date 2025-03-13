d6eb0e9997de631ba552325d9f6c3d86b9d3aede
Marlon Stiven <marlonsteven930@gmail.com>
2025-03-13 11:14:41 -0500

    home-css

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,32 +1,36 @@
 import { useCallback } from "react";
 import useAuthStore from "../../stores/use-auth-store";
-import { useNavigate} from "react-router";
+import { useNavigate } from "react-router";
 import Button from "../../components/button/Button.jsx"
 import "./Home.css";
 
-
 const Home = () => {
-  const { loginGoogleWithPopUp} = useAuthStore();
+  const { loginGoogleWithPopUp } = useAuthStore();
   const navigate = useNavigate();
 
   const handleLogin = useCallback(() => {
-    loginGoogleWithPopUp().then(() =>navigate("/perfil")).catch(() =>navigate("/"))
+    loginGoogleWithPopUp().then(() => navigate("/perfil")).catch(() => navigate("/"))
   }, [loginGoogleWithPopUp, navigate]);
 
   return (
     <>
       <div>
-        <h1>Continua con Google</h1>
-        <Button
-          text="Iniciar sesión"
-          width="150px"
-          height="50px"
-          type="button"
-          title="Iniciar sesíón con Google"
-          onClick={handleLogin}
-        />
+        <div>
+          <h1>Continua con Google</h1>
+          <Button
+            text="Iniciar sesión"
+            width="150px"
+            height="50px"
+            type="button"
+            title="Iniciar sesíón con Google"
+            onClick={handleLogin}
+          />
+        </div>
+        
       </div>
-  
+
+
+
     </>
   );
 };
