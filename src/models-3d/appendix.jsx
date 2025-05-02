01e3e3f5d31e5efd7df72de5e8df4afb8698c1eb
Soto Espitia <sotomilos004@gmail.com>
2025-05-01 21:42:39 -0500

    adcionar luces y sombras

--- src/models-3d/appendix.jsx
+++ src/models-3d/appendix.jsx
@@ -3,15 +3,14 @@ import { useEffect } from "react";
 
 const Appendix3d = (props) => {
    const Appendixmodel = useGLTF("img-3d/Appendix.glb");
-   useEffect(()=>{
-      console.log(Appendix3d);
-   }, [Appendix3d])
-    return( 
-      <mesh>
-         <primitive object={Appendixmodel.scene}/>
-      </mesh>
-    ) ;
 
+   useEffect(() => {
+      console.log(Appendixmodel);
+   }, [Appendixmodel]);
+
+   return (
+      <primitive object={Appendixmodel.scene} {...props} castShadow receiveShadow />
+   );
 };
 
 export default Appendix3d;
