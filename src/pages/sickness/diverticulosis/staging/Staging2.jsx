77412c1dbec973a08c668995793ec66e2ca0e45c
Marlon Stiven <marlonsteven930@gmail.com>
2025-07-10 22:37:13 -0500

    msrz-10/07/2025

---
+++ src/pages/sickness/diverticulosis/staging/Staging2.jsx
@@ -0,0 +1,27 @@
+import { Environment,Sky, } from "@react-three/drei";
+
+const Staging2 = () => {
+  return (
+//    <Environment
+//      preset={null} // Desactiva el preset por defecto
+//      files={[
+//        "px.png", // Ruta absoluta desde public
+//        "nx.png",
+//        "py.png",
+//        "ny.png",
+//        "pz.png",
+//        "nz.png",
+//      ]}
+//      path="staging/hospital/" // Asegúrate de que la ruta termine con /
+//      ground={{
+//        height: 60,
+//        radius: 100,
+//        scale: 4,
+//      }}
+//      background
+//    />
+    <Sky/>
+  );
+};
+
+export default Staging2;
